3b79e0720cb7e64d25f7cb1e834c73be8dfd4530
Cory Alder <cory@buddybuild.com>
2017-11-16 09:27:03 -0800

    add tests

---
+++ QuickAppTests/QuickAppTests.m
@@ -0,0 +1,31 @@
+//
+//  QuickAppTests.m
+//  QuickAppTests
+//
+//  Created by Cory Alder on 2017-11-16.
+//  Copyright © 2017 Cory Alder. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface QuickAppTests : XCTestCase
+
+@end
+
+@implementation QuickAppTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    XCTAssertTrue(true);
+}
+
+@end
